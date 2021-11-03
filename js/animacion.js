a8ddcd3ee9c4cfc5dc811fc9d24665afb9926543
alexarce <a.l.e.x_91@hotmail.com>
2021-11-03 19:05:22 -0300

    final part

---
+++ js/animacion.js
@@ -0,0 +1,269 @@
+// Animacion Jquery agrandar imagen
+
+// Producto 1 
+
+$(document).ready(function(){
+    $("#button1").click(function(){
+        $("p").hide(1000);
+        alert("Acaba de Añadir al carro de compras, el producto: ALLIUM GADIATOR.");
+    });
+});
+
+// Producto 2
+
+$(document).ready(function(){
+    $("#button2").click(function(){
+        $("p").hide(1000);
+        alert("Acaba de Añadir al carro de compras, el producto: Alium Spharocephalum.");
+    });
+});
+                     
+// Producto 3
+
+$(document).ready(function(){
+     $("#button3").click(function(){
+    $("p").hide(1000);
+        alert("Acaba de Añadir al carro de compras, el producto: Amarylis simples.");
+});
+});
+
+
+// Producto 4
+
+$(document).ready(function(){
+    $("#button4").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Amaryllis Doble.");
+    });
+  });
+
+// Producto 5
+
+$(document).ready(function(){
+    $("#button5").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Anemononas.");
+    });
+  });
+
+
+// Producto 6
+
+$(document).ready(function(){
+    $("#button6").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Anemononas.");
+    });
+  });
+
+// Producto 7
+
+$(document).ready(function(){
+    $("#button7").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Iris Holandicas.");
+    });
+  });
+
+
+// Producto 8
+
+$(document).ready(function(){
+    $("#button8").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Jacintos.");
+    });
+  });
+
+// Producto 9
+
+$(document).ready(function(){
+    $("#button9").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Lilium Orientales.");
+    });
+  });
+
+// Producto 10
+
+$(document).ready(function(){
+    $("#button10").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Muscaris.");
+    });
+  });
+
+// Producto 11
+
+$(document).ready(function(){
+    $("#button11").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Narciso.");
+    });
+  });
+
+// Producto 12
+
+$(document).ready(function(){
+    $("#button12").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Ranunculos.");
+    });
+  });
+
+// semillas carro de compras
+
+// Producto 1
+
+$(document).ready(function(){
+  $("#button13").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Cartucho enano.");
+  });
+});
+
+// Producto 2
+
+$(document).ready(function(){
+  $("#button14").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Ciboulet.");
+  });
+});
+
+// Producto 3
+
+$(document).ready(function(){
+  $("#button15").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Dalia decorativa.");
+  });
+});
+
+// Producto 4
+
+$(document).ready(function(){
+  $("#button16").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Pensamientos.");
+  });
+});
+
+// Producto 5
+
+$(document).ready(function(){
+  $("#button17").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Petunias Mix.");
+  });
+});
+
+// Producto 6
+
+$(document).ready(function(){
+  $("#button18").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Cartucho alto.");
+  });
+});
+
+// Producto 7
+
+$(document).ready(function(){
+  $("#button19").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Rayito de Sol.");
+  });
+});
+
+// Producto 8
+
+$(document).ready(function(){
+  $("#button20").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: reyna luisa matsumoto.");
+  });
+});
+
+// Producto 9
+
+$(document).ready(function(){
+  $("#button21").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: manzanillon.");
+  });
+});// Producto 10
+
+$(document).ready(function(){
+    $("#button22").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Maravillas variadas.");
+    });
+  });
+  // Producto 11
+
+$(document).ready(function(){
+  $("#button23").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Manzanilla.");
+  });
+});// Producto 12
+
+$(document).ready(function(){
+    $("#button24").click(function(){
+      $("p").hide(1000);
+      alert("Acaba de Añadir al carro de compras, el producto: Pajaritos.");
+    });
+  });
+
+
+// Sustratos y Articulos de Jardin
+
+
+// Producto 1
+
+$(document).ready(function(){
+  $("#button25").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Aceite de Nem.");
+  });
+});
+
+// Producto 2
+
+$(document).ready(function(){
+  $("#button26").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Sustrato Mix.");
+  });
+});
+
+// Producto 3
+
+$(document).ready(function(){
+  $("#button27").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: Trypack fertilizacion.");
+  });
+});
+
+// Producto 4
+
+$(document).ready(function(){
+  $("#button28").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: fibra de coco.");
+  });
+});
+
+
+// Producto 5
+
+$(document).ready(function(){
+  $("#button29").click(function(){
+    $("p").hide(1000);
+    alert("Acaba de Añadir al carro de compras, el producto: pack 10 Maceteros.");
+  });
+});
+
+
